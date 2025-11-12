2feab5d04fbab876363022ac9305c76a37f62be2
Michal Trojanowski <mtroja98@gmail.com>
2025-11-12 11:52:01 +0100

    ProjectServiceTests

---
+++ Task-System.Tests/service/ProjectServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Task_System.Data;
+using Task_System.Model;
+using Task_System.Model.Request;
+using Task_System;
+using Task_System.Service.Impl;
+using Xunit;
+
+namespace Task_System.Tests;
+
+public class ProjectServiceTests
+{
+    private PostgresqlDbContext GetInMemoryDb()
+    {
+        var options = new DbContextOptionsBuilder<PostgresqlDbContext>()
+            .UseInMemoryDatabase("TestDb")
+            .Options;
+        return new PostgresqlDbContext(options);
+    }
+
+    private ILogger<ProjectService> GetLoggerStub() =>
+        new LoggerFactory().CreateLogger<ProjectService>();
+
+    [Fact]
+    public async Task CreateProject_ShouldAddProject()
+    {
+        var db = GetInMemoryDb();
+        var logger = GetLoggerStub();
+        var service = new ProjectService(db, logger);
+
+        var request = new CreateProjectRequest("TEST", "Test project");
+        var project = await service.CreateProject(request);
+
+        Assert.NotNull(project);
+        Assert.Equal("TEST", project.ShortName);
+        Assert.Equal("Test project", project.Description);
+        Assert.Single(await db.Projects.ToListAsync());
+    }
+}
