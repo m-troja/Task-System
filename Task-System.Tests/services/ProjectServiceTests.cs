a8dae42cb173adcd5435ec38e77481a397f78922
Michal Trojanowski <mtroja98@gmail.com>
2025-11-19 23:37:07 +0100

    ProjectServiceTests

--- Task-System.Tests/services/ProjectServiceTests.cs
+++ Task-System.Tests/services/ProjectServiceTests.cs
@@ -2,19 +2,19 @@
 using Microsoft.Extensions.Logging;
 using Task_System.Data;
 using Task_System.Model;
+using Task_System.Model.IssueFolder;
 using Task_System.Model.Request;
-using Task_System;
 using Task_System.Service.Impl;
 using Xunit;
 
-namespace Task_System.Tests;
+namespace Task_System.Tests.services;
 
 public class ProjectServiceTests
 {
     private PostgresqlDbContext GetInMemoryDb()
     {
         var options = new DbContextOptionsBuilder<PostgresqlDbContext>()
-            .UseInMemoryDatabase("TestDb")
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
             .Options;
         return new PostgresqlDbContext(options);
     }
@@ -36,5 +36,21 @@ public class ProjectServiceTests
         Assert.Equal("TEST", project.ShortName);
         Assert.Equal("Test project", project.Description);
         Assert.Single(await db.Projects.ToListAsync());
+    }
+
+    [Fact]
+    public async Task<Project> GetProjectById_ShouldReturnProject()
+    {
+        var db = GetInMemoryDb();
+        var logger = GetLoggerStub();
+        var service = new ProjectService(db, logger);
+
+        var project = new Model.IssueFolder.Project(2, "TEST2", "Test project2", DateTime.UtcNow);
+        db.Projects.Add(project);
+        await db.SaveChangesAsync();
+        var fetchedProject = await service.GetProjectById(project.Id);
+        Assert.NotNull(fetchedProject);
+        Assert.Equal(project.Id, fetchedProject.Id);
+        return fetchedProject;
     }
 }
